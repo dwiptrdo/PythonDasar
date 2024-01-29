f3a08c5c4eb17f163bc70d0cefa88371f0f0f49f
dwiptrdo <alfredoputradwi@gmail.com>
2024-01-29 14:30:25 +0700

    UpdateDasarPython

---
+++ Dasar Dasar Python/Tuples_&_Set.py
@@ -0,0 +1,23 @@
+# list
+# menggunakan kurung siku
+
+data_list = [1,6,3,8,4,3]
+print(data_list)
+
+# tuples (data yang tidak bisa dirubah/konstan)
+
+data_tuples = (1,6,3,5,2)
+print(data_tuples)
+print(data_tuples[1])
+
+# tidak bisa dilakukan
+# data_tuples[1] = "Ali"
+# data_tuples.append(1)
+
+# sets (mirip dengan list tapi tidak memiliki index)
+
+data_sets = {3,8,3,5,1,4,6,2}
+print(data_sets)
+
+# tidak bisa dilakukan
+# print(data_sets[0])
