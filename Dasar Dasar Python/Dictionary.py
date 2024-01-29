22faed36cfd17b09b10faedac2b0a2b51139c6a0
dwiptrdo <alfredoputradwi@gmail.com>
2024-01-29 14:45:24 +0700

    UpdateDasarPython

---
+++ Dasar Dasar Python/Dictionary.py
@@ -0,0 +1,21 @@
+# list --> array, mengakses dengan menggunakan index
+
+data_list = ['Ali', 'Dwi', 'Aziz']
+
+print(data_list[0])
+
+# dictionary (dict) -> associative array mengakses dengan 
+# identifier -> key
+
+data_dict = {
+    'key':'value',
+    'al':'Ali',
+    'dw':'Dwi',
+    'az':'Aziz',
+    'int': 100,
+    'list': data_list
+}
+
+print(data_dict['az'])
+print(data_dict['int'])
+print(data_dict['list'])
